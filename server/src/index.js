769edc8a83fa5d0f016a8151b334b01ed976d9e0
kiran877 <kirangavvala078@gmail.com>
2026-04-20 16:59:23 +0530

    fixing errors1

--- server/src/index.js
+++ server/src/index.js
@@ -43,7 +43,7 @@ const startServer = async () => {
   initSocket(httpServer);
 
   httpServer.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}.`);
   });
 };
 
